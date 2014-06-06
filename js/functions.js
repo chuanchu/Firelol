eb30569e2f9257246d92f57177e666a9888182d3
chuanchu <juanjose.collado.contreras@gmail.com>
2014-06-06 07:42:53 +0200

    Limpieza ficheros

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,51 @@
+
+/* infousuario*/
+
+function loadJSON(form) {
+    var url = "https://prod.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/" + form.nombre.value + "?api_key=654c8f4a-de83-42b4-a424-ee693a76cbb2";
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).done(function (data) {
+            $("#jid").html(data[ form.nombre.value].id);
+            $("#jname").html(data[ form.nombre.value].name);
+            $("#jprof").html(data[ form.nombre.value].profileIconId);
+            $("#jlevel").html(data[ form.nombre.value].summonerLevel);
+            $("#jdate").html(data[ form.nombre.value].revisionDate);
+            $("#userinfo").show();
+        });
+}
+
+/*****************************/
+
+/*Timers*/
+
+$().ready(function () {
+    $(".timer").click(function () {
+        var div = $(this);
+        var init = $(this).data('time');
+        div.html(init);
+        var timer = setInterval(function () {
+            init = countDown(init, div, timer);
+        }, 1000);
+    });
+});
+
+function countDown(time, div, timerId) {
+    div.html(time--);
+    if (time == 0) {
+        clearInterval(timerId);
+    }
+    return time;
+}
+
+/*********************************/
+
+
+
+
+/*Ultimas Partidas*/
+
 function getid(form)
 {
 
@@ -16,8 +64,6 @@ function getid(form)
 
 
 }
-
-
 
 
 
@@ -66,7 +112,7 @@ function getlastgames(id)
 
 
 
-
+/**********************************/
 
 
 
